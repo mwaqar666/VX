1611d35b499c703a5a94347434e382b736dfa1d8
Muhammad Waqar <mwaqar666@yahoo.com>
2024-08-07 18:50:28 +0200

    First commit

---
+++ projects/vx-config/src/public-api.ts
@@ -0,0 +1,5 @@
+export * from "@vx/config/config.module";
+export * from "@vx/config/exceptions";
+export * from "@vx/config/services";
+export * from "@vx/config/types";
+export * from "@vx/config/validation";
